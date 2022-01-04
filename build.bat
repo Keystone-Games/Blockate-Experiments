f525a3d4ee083fea625b18283a68430a5641eb53
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-01-04 08:09:24 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -11,15 +11,15 @@ echo Progress: 25%%
 echo -lib heaps>hl.hxml
 echo Progress: 38%%
 echo -lib hlsdl>hl.hxml
-
+echo Progress: 50%%
 echo -hl out/blockate.hl>hl.hxml
-
+echo Progress: 63%%
 echo -hl out/blockate.c>hl.hxml
-
+echo Progress: 75%%
 echo -main Main>hl.hxml
-
+echo Progress: 88%%
 echo -D windowSize=1920x1080>hl.hxml
-
+echo Progress: 100%%
 haxe hl.hxml
 
 echo Compilation complete! Press any key to exit...
